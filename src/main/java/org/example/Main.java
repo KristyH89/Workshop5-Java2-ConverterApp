0822737160266a3e32774e78e17fecf14a5630b7
Kristy Heijenk <ksheijenk@hotmail.com>
2026-03-05 09:43:57 +0100

    Added simple flowchart

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,51 @@ public class Main {
 
 
 
+/* Flowchart
+Converter App
+Start
 
+1. Currency Converter
+2. Weight Converter
+3. Time Converter
+4. Exit
+
+Option 1:
+Choose u for USD
+Choose e for EUR
+
+Enter amount in SEK:
+Result = amount * 0,109
+Print result + USD
+
+Enter amount in SEK:
+Result = amount * 0,0935
+Print result + EUR
+
+Option 2:
+Choose k for kg -> G
+Enter kg
+Result = kg * 1000
+Print result + gram
+
+Choose g for gram -> kg
+Enter gram
+Result = gram / 1000
+Print result + kilogram
+
+Option 3:
+Choose H for hours -> minutes
+Enter hours
+Result = hours * 60
+Print result + minutes
+
+Choose m for minutes -> hours
+Enter minutes
+Result = minutes /60
+Print result + hours
+
+
+ */
 
         }
     }
